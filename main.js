646f1a16d66f65458f17c1f5b772dce07932534e
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-09-02 23:08:07 -0300

    FINALIZANDO MQ

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const openModal = document.querySelector('#innerModal');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.cerrarModal')
+const botonEnviar = document.querySelector('#botonContacto')
+const ingresoEmpresa = document.querySelector('#ingresoEmpresa')
+const ingresoOrg = document.querySelector('#ingresoOrg')
 
+
+ingresoEmpresa.addEventListener('click',(e)=>{
+    e.preventDefault();
+    openModal.innerHTML= "Mi Empresa";
+})
+
+ingresoOrg.addEventListener('click',(e)=>{
+    e.preventDefault();
+    openModal.innerHTML= "Mi Organizacion";
+})
+
+botonEnviar.addEventListener('click', (e)=>{
+    e.preventDefault();
+    validar_nombre_completo()
+});
 
 openModal.addEventListener('click', (e)=>{
      e.preventDefault();                             //evita el comportamiento por defecto
@@ -13,3 +31,47 @@ closeModal.addEventListener('click', (e)=>{
     e.preventDefault();                            
    modal.classList.remove("modalShow");
 });
+
+
+
+/* INTENTO DE VALIDACION
+
+function validar(){
+    let nombreCompleto = document.querySelector('#fname').value;
+    let email = document.querySelector('#lname').value;
+    let campoText = document.querySelector('#textArea').value;
+    let textoError = ''
+    if(!validar_vacio(nombreCompleto)){
+       textoError += "EL CAMPO NOMBRE COMPLETO NO PUEDE SER VACIO"
+    }
+    if(!validar_vacio(email)){
+        textoError += "EL CAMPO EMAIL NO PUEDE SER VACIO"
+     }
+     if(!validar_vacio(campoText)){
+        textoError += "EL CAMPO MENSAJE NO PUEDE SER VACIO"
+     }
+
+     if(!validar_numero(nombreCompleto)){
+        textoError += "EL CAMPO NOMBRE NO PUEDE CONTENER NUMEROS"
+     }
+}
+
+function validar_vacio(texto){
+  return texto !==''
+}
+
+function validar_numero(texto){
+    return texto.match(/^[0-9]+$/)
+}
+
+function validar_length(texto){
+    return texto.length >= 250
+}
+
+function validar_mail(texto){
+    let caracteres = texto.split("")
+    return caracteres.includes("@")
+}
+\
+
+*/
